bcf50473e5c7045fc71be198964141afd45c1f52
Cecep Qwords <cecep@qwords.com>
2018-08-23 17:13:49 +0700

    Finishing list

--- app/screens/ProjectScreen/ProjectScreen.js
+++ app/screens/ProjectScreen/ProjectScreen.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
-import { View, Text, FlatList, TouchableHighlight, TouchableOpacity, StyleSheet } from 'react-native';
+import { View, Text, FlatList, TouchableHighlight, TouchableOpacity, StyleSheet, Image, Dimensions } from 'react-native';
+import EStyleSheet from 'react-native-extended-stylesheet';
 import { Constants } from "expo";
+import { Ionicons } from "@expo/vector-icons";
+
+const window = Dimensions.get("window");
+const isSmallDevice = window.width < 375;
 
 import styles from './styles';
 import data from "./data";
@@ -10,12 +15,53 @@ class ProjectScreen extends Component {
         super(props);
         this.state = {
             data: data,
-            stickyHeaderIndices: []
+            loading: false,
+            page: 1,
+            seed: 1,
+            error: null,
+            refreshing: false,
+            stickyHeaderIndices: [],
+            flexOnSubtitle: 0,
         };
     }
 
     componentWillMount() {
         this.setStickyHeader();
+    }
+
+    makeRequest = () => {
+        const { page, seed } = this.state
+        const url = `https://randomuser.me/api/?seed=${seed}&page=${page}&results=20`;
+
+        this.setState({ loading: true })
+
+        fetch(url)
+            .then(response => response.json())
+            .then(res => {
+                this.setState({
+                    // data: page === 1 ? res.results: [...this.state.data, ...res.results],
+                    error: res.error || null,
+                    loading: false,
+                    refreshing: false
+                })
+            })
+            .catch(error => {
+                this.setState({ error, loading: false })
+            })
+    }
+
+    handleRefresh = () => {
+        this.setState({
+            page: 1,
+            seed: this.state.seed + 1,
+            refreshing: true,
+        }, () => this.makeRequest())
+    }
+
+    handleLoadMore = () => {
+        this.setState({
+            page: this.state.page + 1,
+        }, () => this.makeRequest())
     }
 
     setStickyHeader = () => {
@@ -51,13 +97,96 @@ class ProjectScreen extends Component {
                 </View>
             )
         } else if (!item.header) {
+            let isLikes = item.likes || item.likes === 0;
+
+            if (item.message) {
+                return (
+                    <View style={styles.messageContainer}>
+                        <Text style={styles.messageText}>{item.message}</Text>
+                    </View>
+                )
+            }
+
+            const handleSubtitleOnLayout = evt => {
+                // console.log(evt.nativeEvent.layout)
+
+                let layout = evt.nativeEvent.layout;
+                let width = layout.width;
+
+                let deviceWidth = isSmallDevice ? 140 : 375;
+                if (width > deviceWidth) {
+                    this.setState({
+                        flexOnSubtitle: 1
+                    })
+                } else {
+                    // this.setState({
+                    //     flexOnSubtitle: 0
+                    // })
+                }
+            }
+
             return (
                 <TouchableHighlight
-                    style={styles.listContent}
+                    style={styles.touchContainer}
                     onPress={() => alert(item.name)}
+                    onLongPress={() => alert('Long Press')}
                     underlayColor={styles.$underlayColor}
                 >
-                    <Text>{item.name}</Text>
+                    <View>
+                        <View
+                            style={[
+                                { flex: 1 },
+                                styles.listContent,
+                                !item.image && !item.title && !item.subtitle && styles.contentRow,
+                            ]}
+                        >
+                            {/* Only title or maybe with icon */}
+                            {!item.image && !item.title && !item.subtitle && (
+                                <View style={styles.singleContainer}>
+                                    {/* <Ionicons style={{ marginEnd: 10 }} name="ios-mail-outline" size={35} /> */}
+                                    <Text style={styles.singleText} numberOfLines={1}>{item.name}</Text>
+                                </View>
+                            )}
+
+                            {/* Only title and subtitle */}
+                            {item.title && item.subtitle && (
+                                <View style={{
+                                    flex: 1,
+                                    flexDirection: 'row',
+                                    alignItems: 'center'
+                                }}>
+                                    {item.image && (
+                                        <Image resizeMode="cover" source={{ uri: item.image }} style={{ width: 40, height: 40, marginEnd: 10, borderRadius: 4 }} />
+                                        // <Ionicons style={{ marginEnd: 10, alignSelf: 'center', }} name="md-checkmark" size={30} />
+                                    )}
+                                    <View style={{
+                                        flex: 1
+                                    }}>
+                                        <Text style={styles.title} numberOfLines={1}>{item.title}</Text>
+                                        <View style={styles.subtitleContainer}>
+                                            <Text onLayout={handleSubtitleOnLayout} style={[styles.subtitle, isLikes && { flex: this.state.flexOnSubtitle }]} numberOfLines={1}>{item.subtitle}</Text>
+                                            {isLikes && (
+                                                <View style={styles.likesContainer}>
+                                                    <View style={styles.likesCircle} />
+                                                    <Text style={styles.likesText} numberOfLines={1}>{item.likes}</Text>
+                                                    <Text style={styles.likesText} numberOfLines={1}>likes</Text>
+                                                </View>
+                                            )}
+                                        </View>
+                                    </View>
+                                </View>
+                            )}
+
+                            {/* Checkmark */}
+                            {item.checked && <Ionicons style={styles.checkmark} name="md-checkmark" size={20} />}
+                        </View>
+
+                        {/* Line */}
+                        <View style={[
+                            styles.listLine,
+                            // item.image && item.title && item.subtitle && styles.halfLine
+                        ]} />
+                    </View>
                 </TouchableHighlight>
             )
         }
@@ -65,17 +194,14 @@ class ProjectScreen extends Component {
 
     renderSeparator = ({ highlighted, leadingItem }) => {
         // DEBUG: console.log(leadingItem)
-        // TODO:
-        // FIXME:
-        // REVIEW: 
-        // NOTE: 
+        console.log(leadingItem)
         if (leadingItem.header) {
             return null;
         }
         return (
             <View style={{
-                borderBottomColor: '#F4F4F5',
-                borderBottomWidth: StyleSheet.hairlineWidth
+                backgroundColor: '#F4F4F5',
+                height: 1
             }} />)
     }
 
@@ -88,7 +214,11 @@ class ProjectScreen extends Component {
                     renderItem={this.renderItem}
                     keyExtractor={item => item.name}
                     stickyHeaderIndices={this.state.stickyHeaderIndices}
-                    ItemSeparatorComponent={this.renderSeparator}
+                    showsVerticalScrollIndicator={false}
+                    onRefresh={this.handleRefresh}
+                    refreshing={this.state.refreshing}
+                    onEndReached={this.handleLoadMore}
+                    onEndReachedThreshold={0}
                 />
             </View>
         );
--- app/screens/ProjectScreen/styles.js
+++ app/screens/ProjectScreen/styles.js
@@ -3,16 +3,63 @@ import EStyleSheet from 'react-native-extended-stylesheet';
 const styles = EStyleSheet.create({
     $paddingHorizontal: 18,
     $paddingVertical: 15,
-    $background: '$backgroundColor',
-    $underlayColor: '#F4F4F4',
+    $background: '#F8F8F9',
+    $underlayColor: '#b5b8bc',
     $headerColor: '#ADB0B4',
     $active: '#20BE2F',
+    $marginLeftLikes: 5,
 
     text: {
         color: '$headerColor',
         fontWeight: '500',
         fontSize: 11,
         letterSpacing: 1.2,
+    },
+    title: {
+        color: '#000',
+        fontSize: 15,
+        fontWeight: '500',
+        paddingBottom: 2,
+    },
+    subtitle: {
+        color: '$headerColor',
+        fontSize: 13.5,
+        flex: 1,
+    },
+    subtitleContainer: {
+        flex: 1,
+        flexDirection: 'row'
+    },
+    likesContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-around',
+        // width: 55,
+        marginLeft: '$marginLeftLikes',
+    },
+    likesText: {
+        marginLeft: '$marginLeftLikes',
+        color: '$headerColor',
+        fontSize: 14,
+        alignSelf: 'flex-start',
+    },
+    likesCircle: {
+        width: 4,
+        height: 4,
+        borderRadius: 4 / 2,
+        backgroundColor: '#ddd',
+        alignSelf: 'center',
+        justifyContent: 'center',
+    },
+
+    singleContainer: {
+        flex: 1,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'flex-start',
+    },
+    singleText: {
+        fontSize: 16,
     },
     dot: {
         backgroundColor: '$headerColor',
@@ -26,9 +73,23 @@ const styles = EStyleSheet.create({
     nanoSpacer: {
         width: 7
     },
+    listLine: {
+        flex: 1,
+        height: 1,
+        backgroundColor: '#F4F4F5'
+    },
+    fullLine: {
+        marginLeft: 0,
+    },
+    halfLine: {
+        marginLeft: 20,
+    },
 
     listContainer: {
         backgroundColor: '$background',
+    },
+    touchContainer: {
+        backgroundColor: '#fff'
     },
 
     listHeaderContainer: {
@@ -36,7 +97,7 @@ const styles = EStyleSheet.create({
         flexDirection: 'row',
         justifyContent: 'space-between',
         alignItems: 'center',
-        paddingVertical: '$paddingVertical',
+        paddingVertical: 12,
         paddingHorizontal: '$paddingHorizontal',
         backgroundColor: '$background',
     },
@@ -48,8 +109,34 @@ const styles = EStyleSheet.create({
 
     listContent: {
         paddingVertical: '$paddingVertical',
-        backgroundColor: '#fff',
+        backgroundColor: '#ffffff',
         paddingHorizontal: '$paddingHorizontal',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+    },
+    contentRow: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+    },
+    contentColumn: {
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+    },
+    checkmark: {
+        color: '#509BDA',
+        alignSelf: 'center',
+    },
+
+    messageContainer: {
+        flex: 1,
+        paddingHorizontal: '$paddingHorizontal',
+        backgroundColor: 'transparent',
+    },
+    messageText: {
+        color: '$headerColor',
+        paddingTop: 12,
+        paddingBottom: 30,
+        fontSize: 13,
     }
 });
 
